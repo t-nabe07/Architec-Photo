58bd5bc7a947849b6b7c021fa4c6f35ab819acf3
t-nabe07 <watanabe.t.job@gmail.com>
2022-01-12 08:36:53 +0000

    [add]devise-validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,8 +6,8 @@ class Customer < ApplicationRecord
 
   validates :last_name,presence:true
   validates :first_name,presence:true
-  validates :last_name_kana,presence:true
-  validates :first_name_kana,presence:true
+  validates :last_name_kana,presence:true, format: { with: /\A[ｧ-ﾝﾞﾟ]+\z/, message: 'は半角カタカナで入力してください。' }
+  validates :first_name_kana,presence:true, format: { with: /\A[ｧ-ﾝﾞﾟ]+\z/, message: 'は半角カタカナで入力してください。' }
   validates :college_name,presence:true
 
   has_many :productions,dependent: :destroy
