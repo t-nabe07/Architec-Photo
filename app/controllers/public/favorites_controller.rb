c5773c31ad0da1335adb0e370f94a6937dc1f669
t-nabe07 <watanabe.t.job@gmail.com>
2022-01-18 05:46:34 +0000

    [add]relationships.js

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,6 +1,6 @@
 class Public::FavoritesController < ApplicationController
   before_action :set_production
-  before_action :authenticate_customer!   # ログイン中のユーザーのみに許可（未ログインなら、ログイン画面へ移動）
+  before_action :authenticate_user!   # ログイン中のユーザーのみに許可（未ログインなら、ログイン画面へ移動）
 
   def create
     if @production.customer_id != current_customer.id
--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,14 +1,13 @@
 class Public::RelationshipsController < ApplicationController
+  before_action :set_production
   before_action :authenticate_customer!
 
   def create
     current_customer.follow(params[:customer_id])
-    redirect_to request.referer
   end
 
   def destroy
     current_customer.unfollow(params[:customer_id])
-    redirect_to request.referer
   end
 
   def followings
@@ -19,6 +18,11 @@ class Public::RelationshipsController < ApplicationController
   def followers
     customer = Customer.find(params[:customer_id])
     @customers = customer.followings
+  end
+
+  private
+  def set_production
+    @customer = Customer.find(params[:customer_id])
   end
 
 end
