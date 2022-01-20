b6bce785a1ff84c31dae707d206e2aeaf53c30b5
t-nabe07 <watanabe.t.job@gmail.com>
2022-01-20 01:35:17 +0000

    [add]index-rank

--- app/controllers/public/productions_controller.rb
+++ app/controllers/public/productions_controller.rb
@@ -1,5 +1,8 @@
 class Public::ProductionsController < ApplicationController
- 
+  #indexだけは閲覧OKにしたい. production.rbに引っかかる。
+  #before_action :redirect_root, except: :index
+  #エラー出ないように仮で設定しておく
+  before_action :authenticate_customer!
 
   def new
     @production = Production.new
@@ -14,6 +17,8 @@ class Public::ProductionsController < ApplicationController
 
   def index
     @production = Production.page(params[:page]).reverse_order
+    #いいね数の比較メソッド
+    @productions = Production.find(Good.group(:production_id).order('count(production_id) desc').limit(4).pluck(:production_id))
   end
 
   def show
--- app/models/production.rb
+++ app/models/production.rb
@@ -1,11 +1,10 @@
 class Production < ApplicationRecord
-  before_commit :authenticate_customer!
 
   belongs_to :customer, optional: true
   has_many :comments,dependent: :destroy
   has_many :goods,dependent: :destroy
   has_many :favorites,dependent: :destroy
-  #Active Storage用
+  #Active Storage複数投稿用
   has_many_attached :images
 
   #フルネーム(nilの場合を除く）
@@ -17,7 +16,7 @@ class Production < ApplicationRecord
     self.last_name_kana + " " + self.first_name_kana
   end
 
-
+  #いいね表示のための定義
   def gooded_by?(customer)
     goods.where(customer_id: customer.id).exists?
   end
